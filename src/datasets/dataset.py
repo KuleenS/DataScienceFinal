0628e7b8d2c7c1c89a8780e299f1244c8ae11694
Kuleen Sasse <cometku@gmail.com>
2024-04-18 17:52:46 -0400

    Updated dataset with parameters

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,8 +1,10 @@
 from abc import ABC
 
+from typing import Dict, Any, List, Tuple
+
 class Dataset(ABC):
     def __init__(self) -> None:
         super().__init__()
     
-    def generate_dataset(self):
+    def generate_dataset(self) -> Tuple[List[Any], List[float]]:
         pass
