a28f5202939109262c3ed77a2aa35e7cf37e9047
Kuleen Sasse <cometku@gmail.com>
2024-04-20 22:57:15 -0400

    added tsne

---
+++ src/dimension_reduction/tsne.py
@@ -0,0 +1,15 @@
+from typing import List
+from sklearn.manifold import TSNE
+
+from dimension_reduction import DimensionReduction
+
+
+class MultiDimensionalScalingReducer(DimensionReduction):
+
+    def __init__(self, kwargs) -> None:
+        super().__init__(kwargs)
+
+        self.reducer = TSNE(**kwargs)
+    
+    def reduce(self, points: List[List[float]]) -> List[List[float]]:
+        return self.reducer.fit_transform(points)
